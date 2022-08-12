54be11a428f3357f42b631fec8124decd88cdef8
zhangj <m17681567973@163.com>
2022-08-12 10:01:52 +0800

    字典

---
+++ day3.py
@@ -0,0 +1,11 @@
+alien1 = {'color':'green','name':'Mask','shape':'cube','points':10}
+print(alien1['color'])
+new_points = alien1['points']
+print(f"you have earned {new_points} scores!")
+
+#先定义一个空的alien2,再添加键值对
+alien2 = {}
+alien2['color'] = 'red'
+alien2['shape'] = 'cube'
+print(alien2)
+
