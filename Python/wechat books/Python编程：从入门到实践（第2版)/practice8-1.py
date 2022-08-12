963845e1775bfcb390507e972125d86f06dc2e32
zhangj <m17681567973@163.com>
2022-08-12 14:42:06 +0800

    更换项目目录

---
+++ Python/wechat books/Python编程：从入门到实践（第2版)/practice8-1.py
@@ -0,0 +1,6 @@
+"""练习8-1：消息　编写一个名为display_message()的函数，
+它打印一个句子，指出你在本章学的是什么。调用这个函数，确认显示的消息正确无误。"""
+def display_message(content):
+    print(f"in the chapter you have learned {content},well doen!")
+display_message("how to use python func")
+
