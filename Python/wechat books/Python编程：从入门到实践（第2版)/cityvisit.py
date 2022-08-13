94ebf14182dfe7f654fae49472dccf1938af6577
zhangjiang <zhangjiang@163.com>
2022-08-13 13:29:38 +0800

    添加语句

--- Python/wechat books/Python编程：从入门到实践（第2版)/cityvisit.py
+++ Python/wechat books/Python编程：从入门到实践（第2版)/cityvisit.py
@@ -1,9 +1,11 @@
 prompt = "\n请输入一个城市名："
 prompt += "\n如果你想退出，请键入quit\n"
-
+citys = []
 while True:
     city = input(prompt)
     if city == 'quit':
+        print(f"你喜欢的城市如下：{citys}")
         break
     else:
-        print(f"I would like to visit {city.title()} again")
+        citys.append(city)
+        print(f'I would like to visit {city.title()} again')
